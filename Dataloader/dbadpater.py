5ace9214ccb48633487cecfc098640bb230853ea
Aperjump <wangwei_aperion@163.com>
2017-08-17 23:18:25 +0800

    [Problem] : Implementation between iterable and iterator

--- Dataloader/dbadpater.py
+++ Dataloader/dbadpater.py
@@ -10,3 +10,18 @@ class dbadapter(object):
 
     def __init__(self, mongconct):
         self._connection = mongconct
+
+    def gettime(self, begin, end):
+        self._begin = begin
+        self._end = end
+
+    def connect(self):
+        self._resultiter = self._connection.find({"$and" : [{"time" : {"$gte" : self._begin}},
+                                                            {"time" : {"$lte" : self._end }}]})
+    # Confused here, should I just implement next() making dbadapater an iterable class,
+    # or make it a fully-fledged iterator
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return next(self._resultiter)
