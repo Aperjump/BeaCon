27e043a83048cc821250e0c02a83dd77f7903994
Aperjump <wangwei_aperion@163.com>
2017-08-20 12:31:04 +0800

    [Build] : Finish dispatcher, testing...

--- Dataloader/dispatcher.py
+++ Dataloader/dispatcher.py
@@ -7,12 +7,16 @@ import pymongo
 import json
 import multiprocessing as mul
 from Strategy.strategy import strategy
-from dbadpater import dbadapter
+from dbadapter import *
 class atomicdispatcher(mul.Process):
+
+    def start(self, path = None, strategy = None):
+        self.__init__(path, strategy)
+        self.initmongo()
 
     def __init__(self, path = None, strategy = None):
         if path is None:
-            path = "../Storage/stocknames.json"
+            path = "../Strategy/Config/strategy1.json"
         self._stocknamefile = open(path, encoding="utf-8")
         self._configstruct = json.load(self._stocknamefile)
         try:
@@ -20,7 +24,7 @@ class atomicdispatcher(mul.Process):
         except AssertionError as e:
             print("Missing strategy, try to find one! ")
         self._strategy = strategy
-        
+
     def initmongo(self):
         self._client = pymongo.MongoClient(self._configstruct['mongodburl'])
         try:
@@ -28,7 +32,10 @@ class atomicdispatcher(mul.Process):
             self._stockdb = self._db[self._strategy._stock]
         except pymongo.CollectionInvalid as e:
             print("Cannot find the correct database, try it again.")
-        self._iter(self._stockdb)
-        self._iter.gettime(self._strategy._begin, self._strategy._end)
-        self._iter.connect()
+        # init ierators
+        self._dbconnect = dbadapter(self._stockdb, self._strategy._begin, self._strategy._end)
+
+    def run(self):
+        for item in self._dbconnect:
+            self._strategy.OnEvent(item)
 
