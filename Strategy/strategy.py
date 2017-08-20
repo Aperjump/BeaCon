ccc70a6b389cb784a4343439f756a0de0c4fde9b
Aperjump <wangwei_aperion@163.com>
2017-08-20 12:32:04 +0800

    [Build] : Strategy Template Building....

--- Strategy/strategy.py
+++ Strategy/strategy.py
@@ -5,3 +5,8 @@
 class strategy(object):
     def __init__(self):
         self._stock = '600060'
+        self._begin = "2014-01-01"
+        self._end = "2016-04-05"
+
+    def OnEvent(self, item):
+        print(item)
