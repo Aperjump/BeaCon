9b0d3558f6ac77f0133896507c8ef5beb04de461
Aperjump <wangwei_aperion@163.com>
2017-08-21 16:34:15 +0800

    [Build] : Add OnRoadOrderManger class, no test

--- Strategy/strategy.py
+++ Strategy/strategy.py
@@ -7,21 +7,100 @@ import json
 from Strategy.position import *
 from abc import ABCMeta
 
+
+class OnRoadOrder(object):
+
+    def __init__(self):
+        self.secID = "000000"
+        self.price = 10
+        self.vol = 100
+        self.dir = "B"
+        self.count = 0
+        self.alive = False
+
+    def build(self, secID, price, vol, direc):
+        self.secID = secID
+        self.price = price
+        self.vol = vol
+        # dir = 'B' means "Buy"
+        # dir = 'S' means "Sell"
+        self.dir = direc
+        self.count = 0
+        self.alive = True
+
+    def destruct(self):
+        self.secID = "000000"
+        self.price = 10
+        self.vol = 100
+        self.dir = "B"
+        self.count = 0
+        self.alive = False
+
+    def totransactionrecord(self):
+        return TransactionRecord(self.secID, self.price, self.vol, self.dir)
+
+class OnRoadOrderManager(object):
+
+    def __init__(self, position, maxnum = 5, time2cancel = 10):
+        self.position = position
+        self.time2cancel = time2cancel
+        self.recordlist = list()
+        for i in range(maxnum):
+            self.recordlist.append(OnRoadOrder())
+
+    def get(self,OnRoadOrder):
+        positionflag = False
+        for item in self.recordlist:
+            if (item.alive):
+                item.build(OnRoadOrder.secID,
+                           OnRoadOrder.price,
+                           OnRoadOrder.vol,
+                           OnRoadOrder.dir)
+                positionflag = True
+                break
+            else:
+                pass
+        if positionflag:
+            print("Insert Success.")
+        else:
+            print("No available position.")
+
+    def checktrade(self, stockdata):
+        for record in self.recordlist:
+            if stockdata.low <= record.price and stockdata.high >= record.price:
+                self.position.holdrecord(record.totransactionrecord)
+                record.distruct()
+            else:
+                if record.count >= self.time2cancel:
+                    record.distruct()
+                else:
+                    record.count += 1
+
+
 class Strategy(ABCMeta):
-    def __init__(self, path):
+
+    def __init__(cls, path):
         if path is None:
             raise Exception
         else:
-            self._pathfile = open(path, encoding="utf-8")
-            self._strategystruct = json.load(self._pathfile)
-            self._stock = self._strategystruct['stocklist']
-            self._begin = self._strategystruct['starttime']
-            self._end = self._strategystruct['endtime']
+            cls._pathfile = open(path, encoding="utf-8")
+            cls._strategystruct = json.load(cls._pathfile)
+            cls._stock = cls._strategystruct['stocklist']
+            cls._begin = cls._strategystruct['starttime']
+            cls._end = cls._strategystruct['endtime']
+            # record orders which have not closed deal
+            cls.onroad = OnRoadOrderManager()
 
-    def OnEvent(self, item):
+    def setaccount(cls, position = None):
+        if position is None:
+            cls._position = Position(1000000, commision =.001, slipage = .001)
+            cls.onroad = OnRoadOrderManager(position = cls._position)
+
+    def OnEvent(cls, item):
         raise Exception
 
-
+    def sendorder(cls, onroadorder):
+        cls.onroad.get(onroadorder)
 
 
 if __name__ == "__main__":
