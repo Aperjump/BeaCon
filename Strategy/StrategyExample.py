d840c529bdc5f46ad642d6c0632e4345fc39cdb2
Aperjump <wangwei_aperion@163.com>
2017-08-31 18:53:56 +0800

    [Build] : Add one trading example

--- Strategy/StrategyExample.py
+++ Strategy/StrategyExample.py
@@ -1,62 +1,78 @@
 """
-MA Strategy Test:
-Rule:
-If MA(5) upcross MA(10) generate buy signal;
-if MA(5) downcross MA(10) generate sell signal.
-moduleaurther : Wang Wei <wangwei_aperion@163.com>
+Test strategy, cited from other author
+moduleaurther : vnpy
 """
 
 from Strategy.strategy import *
 from collections import deque
 import math
+import talib
 
-class TestStrat(Strategy):
+class Stock(object):
+
+    def __init__(self, secID):
+        self.secID = secID
+        self.buffersize = self.config['buffersize']
+        self.buffernum = 0
+        # Set parameters
+        self.atrLength = 22
+        self.atrMaLength = 10
+        self.rsiLength = 5
+        self.rsiEntry = 16
+
+        self.rsiBuy = 50 + self.rsiEntry
+        self.rsiSell = 50 - self.rsiEntry
+
+        self.closearray = []
+        self.higharrary = []
+        self.lowarrary = []
+        self.atrarrary = []
+        self.atrcount = 0
+        self.atrbuffersize = 100
+
+class TestStrat(StrategyTemplate):
 
     def __init__(self, path = None):
         if path is None:
-            path = "./Strategy/Config/strategy1.json"
-        Strategy.__init__(self, path = path)
-        self.temporder = OnRoadOrder()
-        # Set parameters
-        self.count = 0
-        self.para1 = self._strategystruct["length1"]
-        self.para2 = self._strategystruct["length2"]
-        self.length1 = deque()
-        self.length2 = deque()
+            path = "./Strategy/Config/strategyconfig.json"
+        StrategyTemplate.__init__(self, path = path)
+        self.stocklist = {}
+        for tempstock in self.stock:
+            self.stocklist[tempstock] = Stock(tempstock)
 
-    # help function
-    def paraminit(self, item):
-        if self.count <= self.para1:
-            self.length1.append(item.close)
-            self.length2.append(item.close)
-        elif self.count > self.para1 and self.count <= self.para2:
-            self.updateslow(item)
-            self.length2.append(item.close)
-
-    def updateslow(self, item):
-        self.length1.pop()
-        self.length1.append(item.close)
-        self.MA1 = math.mean(self.length1)
-
-    def updatelong(self, item):
-        self.length2.pop()
-        self.length2.append(item.close)
-        self.MA2 = math.mean(self.length2)
-
-    def OnEvent(self, item):
-        self.count += 1
-        self.onroad.checktrade(item)
-        if self.count <= self.para2:
-            self.paraminit(item)
+    def strategysignal(self, item):
+        singlestock = self.stocklist[item.code]
+        if singlestock.buffernum < singlestock.buffersize:
+            singlestock.closearray.append(item)
+            singlestock.higharrary.append(item)
+            singlestock.lowarrary.append(item)
+            singlestock.buffernum += 1
         else:
-            self.updateslow(item)
-            self.updatelong(item)
-        if (self.MA1 > self.MA2):
-            self.temporder = self.temporder.build(self._stock, "op", "all", "B")
-            self.sendorder(self.temporder)
-
-
-
+            singlestock.closearray.append(item)
+            singlestock.higharrary.append(item)
+            singlestock.lowarrary.append(item)
+            singlestock.closearray[0:singlestock.buffersize - 1] = singlestock.closearray[1:singlestock.buffersize]
+            singlestock.higharrary[0:singlestock.buffersize - 1] = singlestock.higharrary[1:singlestock.buffersize]
+            singlestock.lowarrary[0:singlestock.buffersize - 1] = singlestock.lowarrary[1:singlestock.buffersize]
+            singlestock.atrvalue = talib.ATR(singlestock.higharrary,
+                                             singlestock.lowarrary,
+                                             singlestock.closearray,
+                                             singlestock.atrLength)[-1]
+            singlestock.atrcount += 1
+            if singlestock.atrcount < singlestock.atrbuffersize:
+                singlestock.atrarrary.append(singlestock.atrvalue)
+            else:
+                singlestock.atrarrary.append(singlestock.atrvalue)
+                singlestock.atrarrary[0:singlestock.atrbuffersize - 1] = singlestock.atrarrary[1:singlestock.atrbuffersize]
+                singlestock.atrMa = talib.MA(singlestock.atrarrary,
+                                             singlestock.atrMaLength)[-1]
+                singlestock.rsiValue = talib.RSI(singlestock.closearray,
+                                                 singlestock.rsiLength)[-1]
+                if singlestock.atrvalue > singlestock.atrMa:
+                    if singlestock.rsiValue > singlestock.rsiBuy:
+                        singlestock.sendorder(singlestock.secID, 300, singlestock.closearray[-1] + 0.02, "B")
+                    elif singlestock.rsiValue < singlestock.rsiSell:
+                        singlestock.sendorder(singlestock.secID, 300, singlestock.closearray[-1] - 0.02, "S")
 
 if __name__ == "__main__":
     strategy1 = TestStrat("./Strategy/Config/strategy1.json")
