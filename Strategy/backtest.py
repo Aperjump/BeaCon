b35edfafd9e96d03e456dc4f23c031e52350169b
Aperjump <wangwei_aperion@163.com>
2017-09-02 14:24:33 +0800

    [Build] : Add new log file

--- Strategy/backtest.py
+++ Strategy/backtest.py
@@ -5,10 +5,35 @@ moduleaurther : Wang Wei <wangwei_aperion@163.com>
 """
 from Dataloader.dispatcher import *
 from Strategy.StrategyExample import *
+import logging
+import os
+import shutil
 
 if __name__ == "__main__":
+    #########Clear Logging File##########
+    shutil.rmtree("E:/QuantFrameWork/BeaCon/Logging")
+    os.mkdir("E:/QuantFrameWork/BeaCon/Logging")
+    shutil.rmtree("E:/QuantFrameWork/BeaCon/Strategy/temp")
+    os.mkdir("E:/QuantFrameWork/BeaCon/Strategy/temp")
+    ######### Logging Purpose ##########
+    mainlogger = logging.getLogger("Main")
+    mainlogger.setLevel(logging.INFO)
+    mainhandler = logging.FileHandler("E:/QuantFrameWork/BeaCon/Logging/main.log")
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    mainhandler.setFormatter(formatter)
+    mainlogger.addHandler(mainhandler)
+    streamhandler = logging.StreamHandler()
+    streamhandler.setLevel(logging.INFO)
+    streamhandler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(message)s'))
+    mainlogger.addHandler(streamhandler)
+    ######### Main Strategy ############
     dispatcher = Dispatcher("E:/QuantFrameWork/BeaCon/Dataloader/backtestconfig.json")
+    mainlogger.info("Create Dispatcher")
     strategy = TestStrat("E:/QuantFrameWork/BeaCon/Strategy/Config/strategyconfig.json")
+    mainlogger.info("Create teststrategy")
     strategy.setdispatcher(dispatcher)
+    mainlogger.info("Connect dispacther and teststrategy")
     dispatcher.loaddata()
+    mainlogger.info("Loading Data Successfully")
     dispatcher.run()
+    mainlogger.info("Finish Backtesting")
