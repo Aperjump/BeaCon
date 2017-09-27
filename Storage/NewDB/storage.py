5a3466f05159b90d6000048b0bedcfd20bb12970
Aperjump <wangwei_aperion@163.com>
2017-09-27 21:40:29 +0800

    [Build] : New logger

--- Storage/NewDB/storage.py
+++ Storage/NewDB/storage.py
@@ -2,6 +2,8 @@ import pymongo
 import pandas as pd
 import json
 import os
+import zipfile
+import io
 
 class StorageFromDist(object):
 
@@ -28,6 +30,30 @@ class StorageFromDist(object):
     def loadmin(self):
         os.chdir(self.config['DataDirectory'])
         self.dirfiles = os.listdir()
+        for iterdirfiles in self.dirfiles:
+            print("current month: " + iterdirfiles)
+            datesfiles = os.listdir(iterdirfiles)
+            length = len(datesfiles)
+            n = 0
+            for iterdatesfiles in datesfiles:
+                currentdayfiles = zipfile.ZipFile("./"+ iterdirfiles + "/" + iterdatesfiles, 'r')
+                for filename in currentdayfiles.namelist():
+                    print("reading file: " + filename)
+                    data = currentdayfiles.read(filename)
+                    filename = filename[:-4]
+                    filenamesplit = filename.split(" ")
+                    self._docu = self._col[filenamesplit[1]]
+                    datastrio = io.StringIO(data.decode("gb2312"))
+                    junkword = next(datastrio)
+                    data = pd.read_csv(datastrio)
+                    data.columns = ['code','date','open','high', 'low', 'close', 'volume', 'transac', 'trradenum']
+                    tempdata = data[['code','date', 'open', 'high', 'low', 'close', 'volume']]
+                    pd.options.mode.chained_assignment = None
+                    tempdata['code'] = tempdata['code'].map(lambda x : x[2:])
+                    self._docu.insert_many(json.loads(tempdata.to_json(orient='records')))
+                    print(filename + " finish.")
+                n += 1
+                print(str(round((n / length) * 100,2)) + "%")
 
     def initmongo(self):
         # Connect to Mongodb
@@ -36,5 +62,7 @@ class StorageFromDist(object):
         self._docu = self._col[self.config['mongodocument']]
 
 if __name__ == "__main__":
-    storage = StorageFromDist()
-    storage.load()
+    storage = StorageFromDist("E://QuantFrameWork//BeaCon//Storage//NewDB//configmin.json")
+    storage.loadmin()
+   #storage = StorageFromDist()
+   # storage.load()
